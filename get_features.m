63511df53ede937bd5d44cd98833a496dea6ba26
nadisadr <74685770+nadisadr@users.noreply.github.com>
2021-01-30 16:59:53 +1100

    Update MATLAB code

--- get_features.m
+++ get_features.m
@@ -1,40 +1,37 @@
-function features = get_features(data, header_data,Used_leads) %get_ECGLeads_features
+function features = get_features(data, header_data,leads_idx) %get_ECGLeads_features
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Purpose: Extract features from ECG signals of every lead
 % Inputs:
 % 1. ECG data from available leads (data)
 % 2. Header files including the number of leads (header_data)
-% 3. Recording ID number (i)
+% 3. The available leads index (in data/header file)
 %
 % Outputs:
 % features for every ECG lead:
-% 1. Age 2. Sex 3. root square mean of the ECG leads
+% 1. Age 2. Sex 3. root square mean (RSM) of the ECG leads
 %
 % Author: Nadi Sadr, PhD, <nadi.sadr@dbmi.emory.edu>
 % Version 1.0
 % Date 25-Nov-2020
-% Version 2.0
-% Date 15-Dec-2020
+% Version 2.1
+% Date 25-Jan-2021
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % read number of leads, sample frequency and gain from the header.
 [recording,Total_time,num_leads,Fs,gain,age,sex,Baseline] = extract_data_from_header(header_data);
-
+num_leads = length(leads_idx);
+jj=1;
 try
     % ECG processing
     % Preprocessing: gain and repeat signal patterns
-    for i =1:num_leads
+    for i = [leads_idx{:}]
         % Apply gain and remove baseline
         LeadswGain(i,:)   = (data(i,:)-Baseline(i))* gain(i);
-       
-    end
-        
-    % Edit by Nadi Sadr, 25-Nov-2020
-    for i=1: length(Used_leads) %1:num_leads
-        RMS(i) = sqrt(sum(LeadswGain(i,:)).^2)./length(LeadswGain(i,:));       
-        %Include all features from available leads
-        features(i) = RMS(i);
+        % Extract root square mean (RSM) feature
+        RSM(i) = sqrt(sum(LeadswGain(i,:).^2))./length(LeadswGain(i,:));       
+        features(jj) = RSM(i);
+        jj = jj+1;
     end
     
 catch
